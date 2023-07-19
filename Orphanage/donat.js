f73abe12e9207f6d9c7e70275cf512e97ed1410d
Sagar Agicha <74034242+Sagar-Agicha@users.noreply.github.com>
2023-07-19 22:23:27 +0530

    Update donat.js

--- Orphanage/donat.js
+++ Orphanage/donat.js
@@ -1,12 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAIx5K05JcvPmOhalbB5oP2Zed05tSDlj8",
-  authDomain: "webapp-9e1c8.firebaseapp.com",
-  databaseURL: "https://webapp-9e1c8-default-rtdb.firebaseio.com",
-  projectId: "webapp-9e1c8",
-  storageBucket: "webapp-9e1c8.appspot.com",
-  messagingSenderId: "636035984805",
-  appId: "1:636035984805:web:f6667daa13d134bc550c4c",
-  measurementId: "G-R9291TDLQV"
+  //add ur api details.
 };
 
 // initialize firebase
